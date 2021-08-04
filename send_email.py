a7fd17424cf1189760e4a156dfa4b29acf07be17
Miguel <miguelarrieta02@gmail.com>
2021-08-04 16:18:43 +0100

    Improved script to send a more customised email

--- send_email.py
+++ send_email.py
@@ -1,17 +1,20 @@
 import smtplib
 import sys
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
 
 def main():
-    receiver_email, sender_email, sender_password = sys.argv[1:]
+    html = Template(Path('index.html').read_text())
+    receiver_email, receiver_name, sender_email, sender_password = sys.argv[1:]
 
     email = EmailMessage()
-    email['from'] = 'Some Python Bot'
+    email['from'] = 'Silly Python Bot'
     email['to'] = receiver_email
     email['subject'] = 'You won 1 million dollars!'
 
-    email.set_content('This is the content!')
+    email.set_content(html.substitute(name=receiver_name), 'html')
 
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
